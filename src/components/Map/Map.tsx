50bc6001dd4fe8f3afbd570098cec61eb9d0f5e1
deniSwd <denis_swd@mail.ru>
2022-08-18 18:08:34 +0200

    refactoring leaflet routing file

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,36 +1,19 @@
 import React, {FC} from 'react'
 import s from './Map.module.scss'
-import {MapContainer, Marker, Popup, TileLayer, useMap} from 'react-leaflet'
-import iconMarker from 'leaflet/dist/images/marker-icon.png'
-import iconRetina from 'leaflet/dist/images/marker-icon-2x.png'
-import iconShadow from 'leaflet/dist/images/marker-shadow.png'
-import L from 'leaflet'
-import RoutingMachine from "./Routing";
+import {MapContainer, TileLayer} from 'react-leaflet'
+import RoutingMachine from './Routing'
 
-const icon = L.icon({
-  iconRetinaUrl:iconRetina,
-  iconUrl: iconMarker,
-  shadowUrl: iconShadow
-});
-
-
-
-export const Map:FC = () =>{
- /* const count = useAppSelector(selectCount)
-  const dispatch = useAppDispatch()*/
-
+export const Map: FC = () => {
   return (
     <div className={s.mapWrap}>
-      <MapContainer center={[51.505, -1.09]} zoom={13} style={{ height: '738px'}}>
+      <MapContainer doubleClickZoom={false}
+                    id="ma pId"
+                    zoom={14}
+                    center={[33.5024, 36.2988]}
+                    style={{height: '738px'}}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -1.09]} icon={icon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
         <RoutingMachine/>
       </MapContainer>
     </div>
---
+++ src/components/Map/Routing.ts
@@ -0,0 +1,30 @@
+import {Routing, latLng} from 'leaflet'
+import {createControlComponent} from "@react-leaflet/core"
+import 'leaflet-routing-machine'
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
+
+const createRoutineMachineLayer = () => {
+  return  Routing.control({
+    waypoints: [
+      latLng(33.52001088075479, 36.26829385757446),
+      latLng(33.50546582848033, 36.29547681726967),
+      latLng(33.60546582848033, 36.39547681726967),
+    ],
+    lineOptions: {
+      styles: [{ color: "red", weight: 4 }],
+      extendToWaypoints: true,
+      missingRouteTolerance: 0
+    },
+    show: false,
+    addWaypoints: false,
+    routeWhileDragging: true,
+    fitSelectedRoutes: true,
+    showAlternatives: false
+  })
+}
+
+const RoutingMachine = createControlComponent(createRoutineMachineLayer);
+
+export default RoutingMachine;
+
+
